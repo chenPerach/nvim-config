a367b61f889cd9086b7642a795b19b6cf2a8d61b
chen perach <chen76273@gmail.com>
2022-09-26 18:02:23 +0300

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+require('plugins')
+require('setup.telescope')
+
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.swapfile = false
+
+vim.cmd[[colorscheme gruvbox]]
---
+++ lua/setup/telescope.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap("n","<Space>ff",":Telescope find_files<CR>", {})
+
