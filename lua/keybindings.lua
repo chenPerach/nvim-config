38b8fd4731e2336534555622c4239558e1f87ed9
chen perach <chenperach@protonmail.com>
2022-10-14 17:45:52 +0300

    fix: Moved Telescope keymap to keymap.lua

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,7 +2,12 @@ function execute(command)
     return function() vim.cmd(command) end
 end
 
+-- General editing commands.
 vim.keymap.set({"t", "n", "i"}, "<C-n>", execute("TablineBufferNext"), {})
 vim.keymap.set({"t", "n", "i"}, "<C-p>", execute("TablineBufferPrevious"), {})
 vim.keymap.set({"t", "n", "i"}, "<C-s>", execute("wall"), {})
 vim.keymap.set("n","<Space>h", execute("noh"), {})
+
+-- Telescope.
+vim.api.nvim_set_keymap("n","<Space>ff",":Telescope find_files<CR>", {})
+vim.api.nvim_set_keymap("n","<Space>git",":Telescope git_branches<CR>", {})
--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,3 +1,1 @@
-vim.api.nvim_set_keymap("n","<Space>ff",":Telescope find_files<CR>", {})
-vim.api.nvim_set_keymap("n","<Space>git",":Telescope git_branches<CR>", {})
-vim.api.nvim_set_keymap("n","<Space>m",":Telescope man_pages<CR>", {})
+
