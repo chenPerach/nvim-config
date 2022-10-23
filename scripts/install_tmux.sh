1ab2c1c2f1721651c841ab1bcf43e7720377655d
chenPerach <chenperach@proton.me>
2022-10-23 22:21:05 +0300

    Added tmux themes.

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -4,7 +4,7 @@ GIT_DOMAIN="github.com"
 # install tpm
 git clone git@$GIT_DOMAIN:/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # switch to our config 
-ln -sf $(pwd)/../.tmux.conf ~/.tmux.conf
+ln -sf $HOME/.config/nvim/.tmux.conf ~/.tmux.conf
 
 
 
